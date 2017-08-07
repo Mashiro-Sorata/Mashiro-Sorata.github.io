9c8c1a1190597e4bcedaf7f1225680a368b956a0
Mashiro-Sorata <490328928@qq.com>
2017-08-07 07:28:40 +0000

    Update Static Site

--- js/src/motion.js
+++ js/src/motion.js
@@ -9,6 +9,10 @@ $(document).ready(function () {
   window.onload=function(){
       var bwol=document.body.offsetWidth;
       var live2d=document.getElementById('live2dcanvas');
+      var btn_image=['http://oty1v077k.bkt.clouddn.com/bukagirl.jpg',
+                     'http://oty1v077k.bkt.clouddn.com/jumpgirl.jpg',
+                     'http://oty1v077k.bkt.clouddn.com/%E8%90%8C1.gif'];
+      var btn_index =Math.round(Math.random()*(btn_image.length-1));
       if(bwol < 975){
           $('aside#sidebar').css("display","none");
           $('body').css("paddingLeft","0px");
@@ -19,7 +23,13 @@ $(document).ready(function () {
           $('.fixed_side').css('display','none');
       else
           $('.fixed_side').css('display','block');
-      ap.isMobile && $('.fixed_side').css('display','none');
+      if(ap.isMobile){
+        $('.fixed_side').css('display','none');
+      }
+      else {
+        $('.mybtn').css('background','url(' + btn_image[btn_index] + ') no-repeat center');
+        $('.mybtn').css('background-size','contain');
+      }
   }
 
   window.onresize = function(){
