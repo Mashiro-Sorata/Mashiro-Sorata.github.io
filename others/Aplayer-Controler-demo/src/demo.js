861cc6626a2b3d7ad35bb5349300e4f530fca684
Mashiro-Sorata <490328928@qq.com>
2021-07-28 13:39:08 +0000

    Update Static Site

--- others/Aplayer-Controler-demo/src/demo.js
+++ others/Aplayer-Controler-demo/src/demo.js
@@ -10,46 +10,46 @@ var ap1 = new APlayer({
 	    listmaxheight: '250px',                                             // Optional, max height of play list
 			music: [
 	    {
-	    	title: 'Intro - intro',                                          // Required, music title
-	        author: '西原健一郎',                          // Required, music author
-	        url: 'https://mashirosorata-1254084432.cos.ap-chengdu.myqcloud.com/AP_Demo/%E8%A5%BF%E5%8E%9F%E5%81%A5%E4%B8%80%E9%83%8E%20-%20Intro.mp3',  // Required, music url
-	        pic: 'https://mashirosorata-1254084432.cos.ap-chengdu.myqcloud.com/AP_Demo/cover.jpg'
+	    	title: '廻廻奇譚',                                          // Required, music title
+	        author: 'Eve',                          // Required, music author
+	        url: 'https://music.163.com/song/media/outer/url?id=1484409732.mp3',  // Required, music url
+	        pic: 'http://p1.music.126.net/6fS7bvNGP2Nu2zgyusABZQ==/109951165484066989.jpg?param=130y130'
 	    },
 	    {
-	    	title: 'Natural Relax',
-	    	author: '西原健一郎',
-	    	url: 'https://mashirosorata-1254084432.cos.ap-chengdu.myqcloud.com/AP_Demo/%E8%A5%BF%E5%8E%9F%E5%81%A5%E4%B8%80%E9%83%8E%20-%20Natural%20Relax.mp3',
-	    	pic: 'https://mashirosorata-1254084432.cos.ap-chengdu.myqcloud.com/AP_Demo/cover.jpg'
+	    	title: '夜に駆ける',
+	    	author: 'YOASOBI',
+	    	url: 'https://music.163.com/song/media/outer/url?id=1409311773.mp3',
+	    	pic: 'http://p1.music.126.net/3xWlqwYmfwRFebeiONUpGg==/109951164546210608.jpg?param=130y130'
 	    },
 	    {
-	    	title: 'Get Inside Your Heart feat. Pismo',
-	    	author: '西原健一郎',
-	    	url: 'https://mashirosorata-1254084432.cos.ap-chengdu.myqcloud.com/AP_Demo/%E8%A5%BF%E5%8E%9F%E5%81%A5%E4%B8%80%E9%83%8E%20-%20Get%20Inside%20Your%20Heart%20feat.%20Pismo.mp3',
-	    	pic: 'https://mashirosorata-1254084432.cos.ap-chengdu.myqcloud.com/AP_Demo/cover.jpg'
+	    	title: 'SAKURA',
+	    	author: '生物股长',
+	    	url: 'https://music.163.com/song/media/outer/url?id=407000247.mp3',
+	    	pic: 'http://p2.music.126.net/ios-ijIvUbUyLQcSnMRZnA==/109951163938844912.jpg?param=130y130'
 	    },
 	    {
-	    	title: 'Beautiful Things Remix feat. Amanda Diva - remix',
-	    	author: '西原健一郎',
-	    	url: 'https://mashirosorata-1254084432.cos.ap-chengdu.myqcloud.com/AP_Demo/%E8%A5%BF%E5%8E%9F%E5%81%A5%E4%B8%80%E9%83%8E%20-%20Beautiful%20Things%20Remix%20feat.%20Amanda%20Diva.mp3',
-	    	pic: 'https://mashirosorata-1254084432.cos.ap-chengdu.myqcloud.com/AP_Demo/cover.jpg'
+	    	title: 'ジレンマ',
+	    	author: 'YuNi',
+	    	url: 'https://music.163.com/song/media/outer/url?id=1360967223.mp3',
+	    	pic: 'http://p1.music.126.net/RQ6yBeHPozT73vLLWtcoAw==/109951164021315639.jpg?param=130y130'
 	    },
 	    {
-	    	title: 'Heartbeat',
-	    	author: '西原健一郎',
-	    	url: 'https://mashirosorata-1254084432.cos.ap-chengdu.myqcloud.com/AP_Demo/%E8%A5%BF%E5%8E%9F%E5%81%A5%E4%B8%80%E9%83%8E%20-%20Heartbeat.mp3',
-	    	pic: 'https://mashirosorata-1254084432.cos.ap-chengdu.myqcloud.com/AP_Demo/cover.jpg'
+	    	title: 'flos',
+	    	author: 'YuNi',
+	    	url: 'https://music.163.com/song/media/outer/url?id=1360967181.mp3',
+	    	pic: 'http://p1.music.126.net/GfUCXRWUnGqdM5y0R-JtuQ==/109951164021307387.jpg?param=130y130'
 	    },
 			{
-	    	title: 'Silent Motorway',                                          // Required, music title
-	        author: '西原健一郎',                          // Required, music author
-	        url: 'https://mashirosorata-1254084432.cos.ap-chengdu.myqcloud.com/AP_Demo/%E8%A5%BF%E5%8E%9F%E5%81%A5%E4%B8%80%E9%83%8E%20-%20Silent%20Motorway.mp3',  // Required, music url
-	        pic: 'https://mashirosorata-1254084432.cos.ap-chengdu.myqcloud.com/AP_Demo/cover.jpg'
+	    	title: 'はじめましての気持ちを',                                          // Required, music title
+	        author: 'DAOKO/神山羊',                          // Required, music author
+	        url: 'https://music.163.com/song/media/outer/url?id=1389088985.mp3',  // Required, music url
+	        pic: 'http://p2.music.126.net/l0DJ_4TYuxceXzN0ufB42Q==/109951164347030397.jpg?param=130y130'
 	    },
 			{
-				title: 'Heart Remix feat. Substantial - remix',
-				author: '西原健一郎',
-				url: 'https://mashirosorata-1254084432.cos.ap-chengdu.myqcloud.com/AP_Demo/%E8%A5%BF%E5%8E%9F%E5%81%A5%E4%B8%80%E9%83%8E%20-%20Heart%20Remix%20feat.%20Substantial.mp3',
-				pic: 'https://mashirosorata-1254084432.cos.ap-chengdu.myqcloud.com/AP_Demo/cover.jpg'
+				title: 'ぶれないアイで',
+				author: 'Mitchie M/初音ミク',
+				url: 'https://music.163.com/song/media/outer/url?id=40915595.mp3',
+				pic: 'http://p2.music.126.net/moHg5uQnCkmab_VrZlLRSQ==/2538772352604273.jpg?param=130y130'
 			}]
 });
 
@@ -65,46 +65,46 @@ var ap2 = new APlayer({
 	    listmaxheight: '250px',                                             // Optional, max height of play list
 	    music: [
 		    {
-		    	title: 'Folklove (Inst.) - instrumental',
-		    	author: '西原健一郎',
-		    	url: 'https://mashirosorata-1254084432.cos.ap-chengdu.myqcloud.com/AP_Demo/%E8%A5%BF%E5%8E%9F%E5%81%A5%E4%B8%80%E9%83%8E%20-%20Folklove%20(Inst.).mp3',
-		    	pic: 'https://mashirosorata-1254084432.cos.ap-chengdu.myqcloud.com/AP_Demo/cover.jpg'
+		    	title: '大江戸ジュリアナイト',
+		    	author: 'Mitchie M/初音ミク',
+		    	url: 'https://music.163.com/song/media/outer/url?id=502455385.mp3',
+		    	pic: 'http://p1.music.126.net/Wgi0eBdLS0m5910SuAeqkg==/109951163014923061.jpg?param=130y130'
 		    },
 		    {
-		    	title: 'Sign feat. Jun',
-		    	author: '西原健一郎',
-		    	url: 'https://mashirosorata-1254084432.cos.ap-chengdu.myqcloud.com/AP_Demo/%E8%A5%BF%E5%8E%9F%E5%81%A5%E4%B8%80%E9%83%8E%20-%20Sign%20feat.%20Jun.mp3',
-		    	pic: 'https://mashirosorata-1254084432.cos.ap-chengdu.myqcloud.com/AP_Demo/cover.jpg'
+		    	title: 'ヒバナ',
+		    	author: 'DECO*27/初音ミク',
+		    	url: 'https://music.163.com/song/media/outer/url?id=502455382.mp3',
+		    	pic: 'http://p2.music.126.net/Wgi0eBdLS0m5910SuAeqkg==/109951163014923061.jpg?param=130y130'
 		    },
 		    {
-		    	title: 'Montana',
-		    	author: '西原健一郎',
-		    	url: 'https://mashirosorata-1254084432.cos.ap-chengdu.myqcloud.com/AP_Demo/%E8%A5%BF%E5%8E%9F%E5%81%A5%E4%B8%80%E9%83%8E%20-%20Montana.mp3',
-		    	pic: 'https://mashirosorata-1254084432.cos.ap-chengdu.myqcloud.com/AP_Demo/cover.jpg'
+		    	title: '劣等上等',
+		    	author: '鏡音リン/レン/ギガP',
+		    	url: 'https://music.163.com/song/media/outer/url?id=1294899572.mp3',
+		    	pic: 'http://p1.music.126.net/wOkN2jRerB5de-DPpSu3jg==/109951163423380232.jpg?param=130y130'
 		    },
 				{
-					title: 'Mature Opinion (Inst.) - instrumental',
-					author: '西原健一郎',
-					url: 'https://mashirosorata-1254084432.cos.ap-chengdu.myqcloud.com/AP_Demo/%E8%A5%BF%E5%8E%9F%E5%81%A5%E4%B8%80%E9%83%8E%20-%20Mature%20Opinion%20(Inst.).mp3',
-					pic: 'https://mashirosorata-1254084432.cos.ap-chengdu.myqcloud.com/AP_Demo/cover.jpg'
+					title: '初梦',
+					author: '初音ミク/MusikM',
+					url: 'https://music.163.com/song/media/outer/url?id=515452689.mp3',
+					pic: 'http://p1.music.126.net/JwNOB4fOcpaJGPMChNVvXg==/109951163051193713.jpg?param=130y130'
 				},
 				{
-					title: 'Individual Carnival',
-					author: '西原健一郎',
-					url: 'https://mashirosorata-1254084432.cos.ap-chengdu.myqcloud.com/AP_Demo/%E8%A5%BF%E5%8E%9F%E5%81%A5%E4%B8%80%E9%83%8E%20-%20Individual%20Carnival.mp3',
-					pic: 'https://mashirosorata-1254084432.cos.ap-chengdu.myqcloud.com/AP_Demo/cover.jpg'
+					title: '病名は愛だった',
+					author: 'Neru/鏡音リン/レン',
+					url: 'https://music.163.com/song/media/outer/url?id=505474379.mp3',
+					pic: 'http://p2.music.126.net/0r3rPTmSp_qGMqR31PvXyg==/109951163023679798.jpg?param=130y130'
 				},
 				{
-					title: 'Say You Love Me feat. Tamala',
-					author: '西原健一郎',
-					url: 'https://mashirosorata-1254084432.cos.ap-chengdu.myqcloud.com/AP_Demo/%E8%A5%BF%E5%8E%9F%E5%81%A5%E4%B8%80%E9%83%8E%20-%20Say%20You%20Love%20Me%20feat.%20Tamala.mp3',
-					pic: 'https://mashirosorata-1254084432.cos.ap-chengdu.myqcloud.com/AP_Demo/cover.jpg'
+					title: '39みゅーじっく!',
+					author: '初音ミク/みきとP',
+					url: 'https://music.163.com/song/media/outer/url?id=1333336766.mp3',
+					pic: 'http://p1.music.126.net/MqQyhohd7n0shK-3PaTavQ==/109951163724940250.jpg?param=130y130'
 				},
 				{
-					title: 'Outro - intro',
-					author: '西原健一郎',
-					url: 'https://mashirosorata-1254084432.cos.ap-chengdu.myqcloud.com/AP_Demo/%E8%A5%BF%E5%8E%9F%E5%81%A5%E4%B8%80%E9%83%8E%20-%20Outro.mp3',
-					pic: 'https://mashirosorata-1254084432.cos.ap-chengdu.myqcloud.com/AP_Demo/cover.jpg'
+					title: 'Wah Wah World',
+					author: 'ギガP/Mitchie M',
+					url: 'https://music.163.com/song/media/outer/url?id=1479972357.mp3',
+					pic: 'http://p2.music.126.net/nVsgraaZQIXl88NSgC6Zcg==/109951165323140567.jpg?param=130y130'
 				}]
 });
 
